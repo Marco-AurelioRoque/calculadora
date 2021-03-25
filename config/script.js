f4046847ed5b79bacb6ac423aa4978c93dbe9410
Marco Aurélio Roque de Oliveira Junior <marcoaureliocraft@gmail.com>
2021-03-25 00:24:24 -0300

    Nao finalizado!

--- config/script.js
+++ config/script.js
@@ -1,35 +1,38 @@
+
+
+
 //Valor 1
 var primeiroValor = parseInt(prompt("Digite o primeiro valor: "))
 //Valor 2
 var segundoValor = parseInt(prompt("Digite o segundo valor: "))
 
-//Resultado
-
-
-
+//Operação
 var operacao = prompt("Digite 1 para fazer uma divisão, 2 para multiplicação, 3 para soma e 4 para subtração")
 
 if (operacao == 1) {
 
     var resultado = primeiroValor / segundoValor
-    document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
+    document.write("<p>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</p")
 
-} else if(operacao == 2){
+} else if (operacao == 2){
 
     var resultado = primeiroValor * segundoValor
-    document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
+    document.write("<p>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</p")
 
-} else if(operacao == 3){
+} else if (operacao == 3){
 
     var resultado = primeiroValor + segundoValor
-    document.write("<h2>" + primeiroValor + " + " + segundoValor + " = " + resultado + "</h2>")
+    document.write("<p>" + primeiroValor + " + " + segundoValor + " = " + resultado + "</p")
 
-} else if(operacao == 4){
+} else if (operacao == 4){
 
     var resultado = primeiroValor - segundoValor
-    document.write("<h2>" + primeiroValor + " - " + segundoValor + " = " + resultado + "</h2>")
+    document.write("<p>" + primeiroValor + " - " + segundoValor + " = " + resultado + "</p>")
 
+} else {
+    document.write("<p>Opção invalida.</p>")
 }
+
 
 
 
@@ -49,5 +52,8 @@ if (operacao == 1) {
 //if = se 
 //else = senão
 //else if = senão se
-//document.write é uma função para escrever algo direto na tela da pessoa
+
 // == é uma operação para comparar algo
+// = é uma operação que usamos para atribuir coisas
+
+//document.write é uma função para escrever algo direto na tela da pessoa

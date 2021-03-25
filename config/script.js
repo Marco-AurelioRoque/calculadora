ac82de0cc6b2dfde76a5936f78a23154dfe6ce90
Marco Aurélio Roque de Oliveira Junior <marcoaureliocraft@gmail.com>
2021-03-24 23:41:25 -0300

    If and else...

--- config/script.js
+++ config/script.js
@@ -4,9 +4,36 @@ var primeiroValor = parseInt(prompt("Digite o primeiro valor: "))
 var segundoValor = parseInt(prompt("Digite o segundo valor: "))
 
 //Resultado
-var resultado = primeiroValor * segundoValor
 
-document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
+
+
+var operacao = prompt("Digite 1 para fazer uma divisão, 2 para multiplicação, 3 para soma e 4 para subtração")
+
+if (operacao == 1) {
+
+    var resultado = primeiroValor / segundoValor
+    document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
+
+} else if(operacao == 2){
+
+    var resultado = primeiroValor * segundoValor
+    document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
+
+} else if(operacao == 3){
+
+    var resultado = primeiroValor + segundoValor
+    document.write("<h2>" + primeiroValor + " + " + segundoValor + " = " + resultado + "</h2>")
+
+} else if(operacao == 4){
+
+    var resultado = primeiroValor - segundoValor
+    document.write("<h2>" + primeiroValor + " - " + segundoValor + " = " + resultado + "</h2>")
+
+}
+
+
+
+
 
 
 
@@ -19,4 +46,8 @@ document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado
 
 //Comentarios
 
+//if = se 
+//else = senão
+//else if = senão se
 //document.write é uma função para escrever algo direto na tela da pessoa
+// == é uma operação para comparar algo

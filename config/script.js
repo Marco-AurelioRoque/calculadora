c1a779ed201ffd7f33ec7e2b90305dbec8f1c8f0
Marco Aurélio Roque de Oliveira Junior <marcoaureliocraft@gmail.com>
2021-03-24 23:18:03 -0300

    Quase terminado

---
+++ config/script.js
@@ -0,0 +1,22 @@
+//Valor 1
+var primeiroValor = parseInt(prompt("Digite o primeiro valor: "))
+//Valor 2
+var segundoValor = parseInt(prompt("Digite o segundo valor: "))
+
+//Resultado
+var resultado = primeiroValor * segundoValor
+
+document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
+
+
+
+
+
+
+
+
+
+
+//Comentarios
+
+//document.write é uma função para escrever algo direto na tela da pessoa

a397b64896ddf8e44ab8ba07d41c05f0b6050818
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-05-08 15:11:59 -0300

    update

--- config/script.js
+++ config/script.js
@@ -1,53 +1,23 @@
-var visorResultado = document.querySelector(".visorResultado")
-
-var numeros1 = [numberOne, numberTwo, numberThree, numberFour]
-var numeros2 = [numberOne, numberTwo, numberThree, numberFour]
-
-function numero1() {
-    var numberOne = document.querySelector(".numero1-calculadora")
-
-    numberOne = 1
-    visorResultado.innerHTML = numberOne
-    visorResultado.classList.remove("tiraResultadoVisor")
+function insert(num) {
+    var numero = document.querySelector(".resultado").innerHTML;
+    document.querySelector(".resultado").innerHTML = numero + num
 }
 
-function numero2() {
-    var numberTwo = document.querySelector(".numero2-calculadora")
-
-    numberTwo = 2
-    visorResultado.innerHTML = numberTwo
-    visorResultado.classList.remove("tiraResultadoVisor")
+function c() {
+    document.querySelector(".resultado").innerHTML = "";
 }
-function numero3() {
-    var numberThree = document.querySelector(".numero3-calculadora")
 
-    numberThree = 3
-    visorResultado.innerHTML = numberThree
-    visorResultado.classList.remove("tiraResultadoVisor")
+function back() {
+    var resultado = document.querySelector(".resultado").innerHTML;
+    document.querySelector(".resultado").innerHTML = resultado.substring(0, resultado.length -1)
 }
-function numero4() {
-    var numberFour = document.querySelector(".numero4-calculadora")
 
-    numberFour= 4
-    visorResultado.innerHTML = numberFour
-    visorResultado.classList.remove("tiraResultadoVisor")
-}
-function numero5() {}
-function numero6() {}
-function numero7() {}
-function numero8() {}
-function numero9() {}
-function numero0() {}
-function numeroMais() {
-    var mais = document.querySelector(".sinais-mais")
+function calcular() {
+    var resultado = document.querySelector(".resultado").innerHTML;
 
-}
-function numeroMenos() {}
-function numeroVezes() {}
-function numeroDivisao() {}
-function numeroSoma() {}
-function numeroClear() {
-    var c = document.querySelector(".sinais-clear")
-
-    visorResultado.classList.add("tiraResultadoVisor")
+    if(resultado) {
+        document.querySelector(".resultado").innerHTML = eval(resultado)
+    } else{
+        document.querySelector(".resultado").innerHTML = "Valor invalido"
+    }
 }
